4e09da493e15bb03f1bdc5ceccf9f9d3d99c0c07
J-Ceguerra <james.ceguerra1@DETNSW>
2016-11-22 14:58:51 +1100

    Basic subscription addition and removal
    Adds and removes itself from editor.update delegate without possible
    leading to memory leaks(?)

--- Assets/Editor/Test2.cs
+++ Assets/Editor/Test2.cs
@@ -8,16 +8,37 @@ using UnityEditor;
 [InitializeOnLoad]
 public class Test2 {
 
+    private static bool isSubscribed = false;
+    private static bool debug = true;
+
     static Test2() {
-        Debug.Log("Test2 Constructor");
-        EditorApplication.update += TestUpdate;
+        printDebug("Test2 Constructor");
+        RemoveSubscription();
+        AddSubscription();
     }
 
     private static void TestUpdate() {
+        if (EditorApplication.isCompiling && isSubscribed) {
+            Debug.Log("It was compiling");
+            RemoveSubscription();
+        }
         Debug.Log("Test2 Update");
     }
 
+    private static void AddSubscription() {
+        printDebug("Test2 SubAdded");
+        isSubscribed = true;
+        EditorApplication.update += TestUpdate;
+    }
     private static void RemoveSubscription() {
-        
+        printDebug("Test2 SubRemvd");
+        isSubscribed = false;
+        EditorApplication.update -= TestUpdate;
+    }
+
+    private static void printDebug(string _msg) {
+        if (debug) {
+            Debug.Log(_msg);
+        }
     }
 }
