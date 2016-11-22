b813bb8d90f5f33d9f9557eafa065a56819805be
J-Ceguerra <james.ceguerra1@DETNSW>
2016-11-22 14:48:56 +1100

    Uploading source files
    Uploading source

---
+++ Assets/Editor/Test2.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[InitializeOnLoad]
+public class Test2 {
+
+    static Test2() {
+        Debug.Log("Test2 Constructor");
+        EditorApplication.update += TestUpdate;
+    }
+
+    private static void TestUpdate() {
+        Debug.Log("Test2 Update");
+    }
+
+    private static void RemoveSubscription() {
+        
+    }
+}
